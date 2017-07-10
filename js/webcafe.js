b23612aafd7b4c4dc9652ca360738d6a7286fdd3
atarvis <atarvis@gmail.com>
2017-07-10 09:24:58 +0900

    마지막 마무리

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,23 +1,30 @@
-//서브메뉴를 위한 스크립트//
-$(function(){
-  $(".main-menu>li").hover(
-    function(){/*함수를 실행*/
-      $(this).children("ul").css("display","block");/*선택할 대상*/
-    },
-    function(){
-      $(this).children("ul").css("display","none");
-    }
-  );
+$(function() {
+    // 메인 메뉴 제어를 위한 스크립트
+    $(".main-menu > li").hover(
+        function() {
+            $(this).find("ul").css("display", "block");
+        },
+        function() {
+            $(this).find("ul").css("display", "none");
+        }
+    );
+    $(".main-menu span").focusin(
+        function() {
+            $(this).siblings("ul").css("display", "block");
+        }
+    );
 
-  $(".main-menu>li>span").focusin(
-    function(){
-      $(this).siblings("ul").css("display","block");
-    }
-  );
-  
     // 탭메뉴 제어를 위한 자바스크립트
     $(".board h2").focus(function() {
         $(this).parent().addClass("act")
             .siblings().removeClass("act");
+    });
+
+    //    관련사이트 키보드 포커스 시 클래스 추가 및 제거
+    $(".related-list a").focusin(function() {
+        $(this).parents(".related-list").addClass("list-act");
+    });
+    $(".related-list a").focusout(function() {
+        $(this).parents(".related-list").removeClass("list-act");
     });
 });
